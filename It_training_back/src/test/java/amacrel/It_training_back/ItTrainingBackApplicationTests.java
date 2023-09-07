6d393397a73a75a12932805793b5ff413ccd9fb0
Alexandre <alexandre.macrel@gmail.com>
2023-09-07 11:51:50 +0200

    First commit - initialize project

---
+++ It_training_back/src/test/java/amacrel/It_training_back/ItTrainingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package amacrel.It_training_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItTrainingBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
